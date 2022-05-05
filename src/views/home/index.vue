866ed06aa9580f91c7ab69709271d269d5e02346
Ronin <2254314552@qq.com>
2022-05-05 18:25:19 +0800

    提交

---
+++ src/views/home/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+
+const sss = ref<HTMLDivElement>();
+onMounted(() => {
+  sss.value?.click()
+});
+
+function handleClick() {
+  console.log('per');
+
+}
+</script>
+
+<template>
+  <div ref="sss" @click="handleClick"></div>
+</template>
